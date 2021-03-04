4b56c08627f172400b90a8d2f206b191397ee81d
heum-ji <dragon2009t@gmail.com>
2021-03-04 09:38:43 +0900

    03.03(수) 수업 정리 누락 추가
    - collection 프로젝트 index 누락

---
+++ Lecture/Collection/src/kr/or/iei/run/ColStart.java
@@ -0,0 +1,12 @@
+package kr.or.iei.run;
+
+import kr.or.iei.func.ListTest;
+
+public class ColStart {
+
+	public static void main(String[] args) {
+		ListTest lt = new ListTest();
+		lt.listTest2();
+	}
+
+}
